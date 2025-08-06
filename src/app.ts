46ae980a555b7afff742c04a14d77f4778be4ae1
Azad Hossan Munna <azadhossanmunna1209@gmail.com>
2025-08-06 10:31:25 +0600

    part-1 done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
 // import {Server} from "http";
-import express, { Request, Response } from "express"
+import express, {  Request, Response } from "express"
+// import { UserRoutes } from "./app/modules/user/user.route"
 // import mongoose from "mongoose"
+import cors from "cors"
+import { router } from "./app/routes"
+// import { envVars } from "./app/config/env"
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler"
+import notFound from "./app/middlewares/notfound"
 
 const app=express()
+
+app.use(express.json())
+app.use(cors())
+app.use("/api/v1",router)
+
 
 app.get("/",(req:Request,res:Response)=>{
     res.status(200).json({
         message:"Welcome to Tour Management System Backend"
     })
 })
+
+app.use(globalErrorHandler)
+app.use(notFound)
+   
 
 export default app
