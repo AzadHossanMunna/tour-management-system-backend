60e3da3be00daad39d77275143ad9dfed7b649da
Azad Hossan Munna <azadhossanmunna1209@gmail.com>
2025-08-07 17:49:46 +0600

    part 3 done

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,39 @@
-// import {Server} from "http";
-import express, {  Request, Response } from "express"
-// import { UserRoutes } from "./app/modules/user/user.route"
-// import mongoose from "mongoose"
-import cors from "cors"
-import { router } from "./app/routes"
-// import { envVars } from "./app/config/env"
-import { globalErrorHandler } from "./app/middlewares/globalErrorHandler"
-import notFound from "./app/middlewares/notfound"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import expressSession from "express-session";
+import passport from "passport";
+import { envVars } from "./app/config/env";
+import "./app/config/passport";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notfound";
+import { router } from "./app/routes";
 
-const app=express()
+const app = express()
 
+
+app.use(expressSession({
+    secret: envVars.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cookieParser())
 app.use(express.json())
 app.use(cors())
-app.use("/api/v1",router)
 
+app.use("/api/v1", router)
 
-app.get("/",(req:Request,res:Response)=>{
+app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
-        message:"Welcome to Tour Management System Backend"
+        message: "Welcome to Tour Management System Backend"
     })
 })
 
+
 app.use(globalErrorHandler)
+
 app.use(notFound)
-   
 
 export default app
