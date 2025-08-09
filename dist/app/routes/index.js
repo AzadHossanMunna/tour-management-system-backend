4863b230010bfdbe3126f18cad0fa858820daab0
Azad Hossan Munna <azadhossanmunna1209@gmail.com>
2025-08-09 09:38:17 +0600

    backend done

---
+++ dist/app/routes/index.js
@@ -0,0 +1,49 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+const auth_route_1 = require("../modules/auth/auth.route");
+const booking_route_1 = require("../modules/booking/booking.route");
+const division_route_1 = require("../modules/division/division.route");
+const payment_route_1 = require("../modules/payment/payment.route");
+const tour_route_1 = require("../modules/tour/tour.route");
+const user_route_1 = require("../modules/user/user.route");
+exports.router = (0, express_1.Router)();
+const moduleRoutes = [
+    {
+        path: "/user",
+        route: user_route_1.UserRoutes
+    },
+    {
+        path: "/auth",
+        route: auth_route_1.AuthRoutes
+    },
+    {
+        path: "/division",
+        route: division_route_1.DivisionRoutes
+    },
+    {
+        path: "/tour",
+        route: tour_route_1.TourRoutes
+    },
+    {
+        path: "/booking",
+        route: booking_route_1.BookingRoutes
+    },
+    {
+        path: "/payment",
+        route: payment_route_1.PaymentRoutes
+    }
+    // {
+    //     path: "/tour",
+    //     route: TourRoutes
+    // },
+];
+moduleRoutes.forEach((route) => {
+    exports.router.use(route.path, route.route);
+});
+// router.use("/user", UserRoutes)
+// router.use("/tour", TourRoutes)
+// router.use("/division", DivisionRoutes)
+// router.use("/booking", BookingRoutes)
+// router.use("/user", UserRoutes)
